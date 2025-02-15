bc833b725f1913357f058fa7a8c8a14218db1948
Vsomera <vsomera77@outlook.com>
2025-02-14 18:36:17 -0800

    attached s3 policies to iam roles

--- access.tf
+++ access.tf
@@ -1,3 +1,6 @@
+### GRANTS EC2 ACCESS TO S3 BUCKET
+
+// policy to grant ec2 access to s3
 resource "aws_iam_policy" "s3_access_policy" {
   name        = "mc-s3-bucket-access"
   description = "Allows mc ec2 instance to access S3 bucket"
@@ -5,3 +8,21 @@ resource "aws_iam_policy" "s3_access_policy" {
   policy = file("${path.module}/config/s3_policy.json")
 }
 
+// iam role for ec2 instance
+resource "aws_iam_role" "mc_ec2_role" {
+  name = "mc-ec2-role"
+
+  assume_role_policy = file("${path.module}/config/ec2_role.json")
+}
+
+// attaches iam policy to the iam role
+resource "aws_iam_role_policy_attachment" "mc_s3_policy_attach" {
+  role       = aws_iam_role.mc_ec2_role.name
+  policy_arn = aws_iam_policy.s3_access_policy.arn
+}
+
+// instance profile to attach to ec2 instance
+resource "aws_iam_instance_profile" "mc_instance_profile" {
+  name = "mc-ec2-instance-profile"
+  role = aws_iam_role.mc_ec2_role.name
+}
