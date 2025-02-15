098f07c8a973a9cc1e21b713342a36b8988347e4
Vsomera <vsomera77@outlook.com>
2025-02-14 22:46:06 -0800

    code clean up

--- scripts/user_data.sh
+++ scripts/user_data.sh
@@ -2,37 +2,31 @@
 
 # Update and install dependencies
 sudo apt update -y
-sudo apt install -y openjdk-8-jdk unzip awscli screen
+sudo apt install -y openjdk-17-jdk unzip awscli screen
+sudo apt install -y libc6-x32 libc6-i386
 
 # Create Minecraft directory and set permissions
 sudo mkdir -p /minecraft
-sudo chown -R ec2-user:ec2-user /minecraft
+sudo chown -R ubuntu:ubuntu /minecraft
 cd /minecraft
 
 # Download Forge installer and Mods
-aws s3 cp s3://server-bucket-mc/setup/forge-1.20.1-47.3.0-installer.jar /minecraft/forge-installer/
+aws s3 cp s3://server-bucket-mc/setup/forge-1.20.1-47.3.0-installer.jar /minecraft/
 aws s3 cp s3://server-bucket-mc/setup/mods.zip /minecraft/mods.zip
+aws s3 cp s3://server-bucket-mc/setup/config.zip /minecraft/config.zip
+
+# Unzip folders
+unzip mods.zip
+unzip config.zip
+
+# Agree to EULA
+echo '#By changing the setting below to TRUE you are indicating your agreement to our EULA (https://account.mojang.com/documents/minecraft_eula).
+#Mon Aug 06 18:11:14 UTC 2018
+eula=true' > eula.txt
 
 # Install forge server
 java -jar forge-1.20.1-47.3.0-installer.jar --installServer
 
-# Unzip mods
-unzip mods.zip
+### edit run.sh and user_jvm_args.txt
 
-# Agree to EULA
-echo "eula=true" > eula.txt
-
-# Download and setup systemd service
-sudo aws s3 cp s3://server-bucket-mc/setup/minecraft.service /etc/systemd/system/minecraft.service
-sudo chmod 644 /etc/systemd/system/minecraft.service
-
-# Reload systemd and start service
-sudo systemctl daemon-reload
-
-sudo service minecraft start
-
-screen -S mcserver
-
-java -Xms2G -Xmx8G -XX:+UseG1GC -jar minecraft_server.1.20.1.jar nogui
-
-### TODO OPTIMIZE ARGUMENTS
+./run.sh
